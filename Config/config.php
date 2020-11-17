00e31ba67eda5eab41a582c3ddecf4d571ee6e22
Mark Mulder <mistermark77@gmail.com>
2020-11-17 23:32:49 +0100

    mautic 3 compatible

--- Config/config.php
+++ Config/config.php
@@ -57,6 +57,24 @@ return [
         'integrations' => [
             'mautic.integration.beefree' => [
                 'class' => \MauticPlugin\MauticBeefreeBundle\Integration\BeefreeIntegration::class,
+                'arguments' => [
+                    'event_dispatcher',
+                    'mautic.helper.cache_storage',
+                    'doctrine.orm.entity_manager',
+                    'session',
+                    'request_stack',
+                    'router',
+                    'translator',
+                    'logger',
+                    'mautic.helper.encryption',
+                    'mautic.lead.model.lead',
+                    'mautic.lead.model.company',
+                    'mautic.helper.paths',
+                    'mautic.core.model.notification',
+                    'mautic.lead.model.field',
+                    'mautic.plugin.model.integration_entity',
+                    'mautic.lead.model.dnc',
+                ],
             ],
         ],
         'repositories' => [
